352acfd0ea295f7dab6c38819aebcd9f9e4247f4
MAziz-0 <maastudentacc@gmail.com>
2021-10-24 06:33:08 +0000

    Stripe payments validation

--- checkout/views.py
+++ checkout/views.py
@@ -7,22 +7,34 @@ from bag.contexts import bag_contents
 
 import stripe
 
+
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
- 
+
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing!')
+        
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_live_51JndsfASy6kxKsGoqYLLQ3pB1h2PzaPNPI81vCPm8uSuj178itGa0YAllRJEnR0Mr9tkMBDX9j0PqJNjeg0vUbiS00eKcB5Ue5',
-        'client_secret':'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
