1d7174ff4f2cb89d8f3314e1a6b90cb6fab3c659
MAziz-0 <maastudentacc@gmail.com>
2021-10-29 18:45:59 +0000

    Changed requirements file

--- merge_store/settings.py
+++ merge_store/settings.py
@@ -16,17 +16,17 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=4=&x763up9u8&z1)ej1l@$glls9=)fbkp4_t*pcenn5nk^al!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['merge-store.herokuapp.com', 'localhost']
 
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
     # Other
     'crispy_forms',
-    'profiles',
     'storages',
 ]
 
