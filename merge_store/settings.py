e377a593204dfd21cb6d3264fbd477f4a7f2233b
MAziz-0 <maastudentacc@gmail.com>
2021-10-29 08:32:25 +0000

    Initiated heroku deployment

--- merge_store/settings.py
+++ merge_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -117,11 +118,15 @@ WSGI_APPLICATION = 'merge_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://fuowqedlsruahd:b19fa336e63a483d2705c08e425a43dbc34f0df81744cbaf79ebe59d40dc9308@ec2-52-17-1-206.eu-west-1.compute.amazonaws.com:5432/d3aqi0rq0g5chb')
 }
 
 
