b9cc94205c5e5ed5ae5cf3bfd794fd49b8587c98
MAziz-0 <maastudentacc@gmail.com>
2021-11-11 14:13:33 +0000

    Created a custom contact up for customers to request more information via a form

--- merge_store/settings.py
+++ merge_store/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '=4=&x763up9u8&z1)ej1l@$glls9=)fbkp4_t*pcenn5nk^al!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['merge-store.herokuapp.com', 'localhost']
 
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     # Other
     'crispy_forms',
     'storages',
+    'contact',
 ]
 
 MIDDLEWARE = [
