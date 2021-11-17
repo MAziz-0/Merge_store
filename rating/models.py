0b79e880efb773c4c298172af0bd24785539199f
MAziz-0 <maastudentacc@gmail.com>
2021-11-17 10:46:05 +0000

    Made changes to models, css and html template for Review rating app

--- rating/models.py
+++ rating/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.conf import settings
 from products.models import Product
 from profiles.models import UserProfile
-
+from django.db.models import Avg, Count
 
 
 class ReviewRating(models.Model):
@@ -14,7 +14,20 @@ class ReviewRating(models.Model):
     rating = models.FloatField()
     ip = models.CharField(max_length=20, blank=True)
     status = models.BooleanField(default=True)
-    
+
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
 
     def __str__(self):
