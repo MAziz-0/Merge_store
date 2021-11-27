534d899e9976af114ceb0b771996da2322d7b405
MAziz-0 <maastudentacc@gmail.com>
2021-11-27 16:17:00 +0000

    Imported ReviewRating model into product detail page

--- products/views.py
+++ products/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-from rating.models import ReviewRating
-
 from .models import Product, Category
+from rating.models import ReviewRating
 from .forms import ProductForm
 # Create your views here.
 
